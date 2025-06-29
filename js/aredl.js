3db53574c0938509129601196503b645c2ad993f
Sanchir Zagasaa <111075711+Saturn4075@users.noreply.github.com>
2025-06-30 00:38:40 +0900

    Update aredl.js

--- js/aredl.js
+++ js/aredl.js
@@ -1,6 +1,6 @@
 export async function getPositionByName(targetName) {
   const response = await fetch("https://api.aredl.net/api/aredl/levels");
   const data = await response.json();
-  const level = data.find(l => l.name.toLowerCase() === targetName.toLowerCase());
+  const level = data.find(l => l.name === targetName);
   return level ? level.position : null;
 }
