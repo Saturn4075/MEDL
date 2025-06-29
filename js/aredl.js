6bdef6ee7830095c1309e52bf76d13c3eb3fdccc
Sanchir Zagasaa <111075711+Saturn4075@users.noreply.github.com>
2025-06-29 23:01:45 +0900

    Update aredl.js

--- js/aredl.js
+++ js/aredl.js
@@ -1,7 +1,6 @@
 export async function getPositionByName(targetName) {
-  const request = "https://api.aredl.net/api/aredl/levels";
-  const response = await fetch(request);
+  const response = await fetch("https://api.aredl.net/api/aredl/levels");
   const data = await response.json();
-  const level = data.find(level => level.name.toLowerCase() === targetName.toLowerCase());
+  const level = data.find(l => l.name.toLowerCase() === targetName.toLowerCase());
   return level ? level.position : null;
 }
