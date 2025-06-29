3df521f9edfab0dc4d09df18a8758d5f52d437ba
Sanchir Zagasaa <111075711+Saturn4075@users.noreply.github.com>
2025-06-30 00:19:34 +0900

    Update score.js

--- js/score.js
+++ js/score.js
@@ -105,20 +105,3 @@ export async function scoreCached(levelName) {
 
   return pointsLevel(maxPoints, minPoints, position, upperLimit, lowerLimit);
 }
-
-export function round(num) {
-    if (!('' + num).includes('e')) {
-        return +(Math.round(num + 'e+' + scale) + 'e-' + scale);
-    } else {
-        var arr = ('' + num).split('e');
-        var sig = '';
-        if (+arr[1] + scale > 0) {
-            sig = '+';
-        }
-        return +(
-            Math.round(+arr[0] + 'e' + sig + (+arr[1] + scale)) +
-            'e-' +
-            scale
-        );
-    }
-}
